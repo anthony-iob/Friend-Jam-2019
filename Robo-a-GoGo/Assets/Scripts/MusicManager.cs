a991d6b2708dc9c52c73464c68565d9a9a73eedc
Jeremy <47025513+soundconclusion@users.noreply.github.com>
2019-11-24 18:34:47 +1100

    MusicManager
    Music is now IN

---
+++ Robo-a-GoGo/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioSource battleMusic;
+
+    public AudioClip introTrack;
+    public AudioClip loopTrack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        battleMusic.clip = introTrack;
+        battleMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!battleMusic.isPlaying)
+        {
+            battleMusic.clip = loopTrack;
+            battleMusic.Play();
+            battleMusic.loop = isActiveAndEnabled;
+           
+        }
+            
+    }
+}
